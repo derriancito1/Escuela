d8f24e014a1b42af1d2a4d7cf7b99884b27525c1
Jonathan Rodriguez Ramirez <jonathan.voices@gmail.com>
2022-08-16 18:28:03 -0500

    Hello world .NET Core 6 Lectura y escritura

--- consoleapp/Program.cs
+++ consoleapp/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Por favor ingresa un nombre");
+string nombre = Console.ReadLine();
+Console.WriteLine("Por favor ingrese su cargo");
+string cargo = Console.ReadLine();
+Console.WriteLine("Por favor ingrese su edad");
+int edad = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Mi nombre es {nombre} mi cargo es {cargo} y tengo {edad}");
