3159d7a4d5179c9357c6d3fe73ea6aa0d8a2dfb8
Jonathan Rodriguez Ramirez <jonathan.voices@gmail.com>
2022-08-18 13:32:40 -0500

    Class Cursos

--- Program.cs
+++ Program.cs
@@ -3,4 +3,14 @@
 var escuela = new Escuela("Platzi Academy", 2012, "Colombia", "Bogota");
 var escuela2 = new Escuela("Platzi Academy", 2012, TiposEscuela.PreEscolar, ciudad:"Bogota");
 escuela.tipoEscuela = TiposEscuela.Primaria;
+
+var curso1 = new Curso() {nombre="101"};
+var curso2 = new Curso() {nombre="201"};
+var curso3 = new Curso() {nombre="301"};
+
 Console.WriteLine(escuela +""+ escuela2);
+System.Console.WriteLine("====================");
+Console.WriteLine(curso1.nombre + ", " + curso1.uniqueId);
+Console.WriteLine($"{curso2.nombre}, {curso2.uniqueId}");
+Console.WriteLine(curso3);
+
