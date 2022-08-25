8550310642d89d93fe59e6682b1ba04c4202cda1
Jonathan Rodriguez Ramirez <jonathan.voices@gmail.com>
2022-08-24 19:17:53 -0500

    Agregar listas y funcion de remover

--- Program.cs
+++ Program.cs
@@ -1,19 +1,46 @@
 ﻿using CoreEscuela.Entidades;
 using static System.Console;
+using System.Collections.Generic;
+using System;
 
 var escuela = new Escuela("Platzi Academy", 2012, "Colombia", "Bogota");
 var escuela2 = new Escuela("Platzi Academy", 2012, TiposEscuela.PreEscolar, ciudad: "Bogota");
 escuela.tipoEscuela = TiposEscuela.Primaria;
 
-escuela.Cursos = new Curso[] {
+/*escuela.Cursos = new Curso[] {
     new Curso() { nombre = "101" },
     new Curso() { nombre = "201" },
     new Curso() { nombre = "301" }
-};
+};*/
+
+
 
 //arregloCursos[0] = new Curso() { nombre = "101" };
 //arregloCursos[1] = new Curso() { nombre = "201" };
 //arregloCursos[2] = new Curso() { nombre = "301" };
+
+escuela.Cursos = new List<Curso>()
+{
+    new Curso() { nombre = "101", jornada=TiposJornada.Mañana},
+    new Curso() { nombre = "201", jornada=TiposJornada.Mañana},
+    new Curso() { nombre = "301", jornada=TiposJornada.Mañana}
+};
+
+escuela.Cursos.Add(new Curso{nombre="102", jornada=TiposJornada.Tarde});
+escuela.Cursos.Add(new Curso(){nombre="202", jornada=TiposJornada.Tarde});
+
+var otraColeccion = new List<Curso>()
+{
+    new Curso() { nombre = "401", jornada=TiposJornada.Mañana},
+    new Curso() { nombre = "501", jornada=TiposJornada.Mañana},
+    new Curso() { nombre = "502", jornada=TiposJornada.Mañana}
+};
+
+//Remover miembros de la coleccion 
+otraColeccion.Clear();
+
+escuela.Cursos.AddRange(otraColeccion);
+
 
 imprimirCursosEscuela(escuela);
 
