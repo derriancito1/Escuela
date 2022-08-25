266847462e99e5c15ab7daf1ed2d10f9c9d662e9
Jonathan Rodriguez Ramirez <jonathan.voices@gmail.com>
2022-08-25 18:07:08 -0500

    Datos de prueba

--- App/EscuentaEngine.cs
+++ App/EscuentaEngine.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 namespace CoreEscuela.Entidades
 {
     public class EscuelaEngine
@@ -13,6 +14,47 @@ namespace CoreEscuela.Entidades
         {
             Escuela = new Escuela("Platzi Academy", 2012, TiposEscuela.Primaria, ciudad: "Bogota", pais: "Colombia");
 
+            CargarCursos();
+            CargarAsignaturas();
+            CargarEvaluaciones();
+
+        }
+
+        private void CargarEvaluaciones()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CargarAsignaturas()
+        {
+            foreach (var curso in Escuela.Cursos)
+            {
+                var listaAsignaturas = new List<Asignatura>()
+                {
+                    new Asignatura{nombre="Matematicas"},
+                    new Asignatura{nombre="Educacion Fisica"},
+                    new Asignatura{nombre="Castellano"},
+                    new Asignatura{nombre="Ciencias Naturales"}
+                };
+                curso.Asignaturas = listaAsignaturas;
+            }
+        }
+
+        private List<Alumno> GenerarAlumnosAlAzar(int cantidad)
+        {
+            string[] nombre1 = { "Alba", "Felipa", "Eusebio", "Farid", "Donald", "Alvaro", "Nicolás" };
+            string[] apellido1 = { "Ruiz", "Sarmiento", "Uribe", "Maduro", "Trump", "Toledo", "Herrera" };
+            string[] nombre2 = { "Freddy", "Anabel", "Rick", "Murty", "Silvana", "Diomedes", "Nicomedes", "Teodoro" };
+
+            var listaAlumnos = from n1 in nombre1
+                               from n2 in nombre2
+                               from a1 in apellido1
+                               select new Alumno{nombre=$"{n1} {n2} {a1}"};
+            return listaAlumnos.OrderBy((Al)=>Al.uniqueId).Take(cantidad).ToList();
+        }
+
+        private void CargarCursos()
+        {
             Escuela.Cursos = new List<Curso>()
                             {
                                 new Curso() { nombre = "101", jornada=TiposJornada.Mañana},
@@ -22,6 +64,13 @@ namespace CoreEscuela.Entidades
                                 new Curso() { nombre = "501", jornada=TiposJornada.Tarde}
                             };
 
+            Random rnd = new Random();
+            
+            foreach (var curso in Escuela.Cursos)
+            {
+                int cantidadRandom = rnd.Next(5,20);
+                curso.Alumnos = GenerarAlumnosAlAzar(cantidadRandom);
+            }
         }
     }
 }
