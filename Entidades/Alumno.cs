4331a6d1f1a4216a97cd2e170b556dc09235cca3
Jonathan Rodriguez Ramirez <jonathan.voices@gmail.com>
2022-08-25 19:03:21 -0500

    Asignacion Aleatoria de calificaciones

--- Entidades/Alumno.cs
+++ Entidades/Alumno.cs
@@ -1,10 +1,22 @@
 using System;
+using System.Collections.Generic;
 namespace CoreEscuela.Entidades
 {
     public class Alumno 
     {
         public string uniqueId {get; private set;}
         public string nombre { get; set;}
-        public Alumno()=> uniqueId = Guid.NewGuid().ToString();
+        
+        public List<Evaluacion> Evaluacion { get; set; }
+
+        
+
+        public Alumno()
+        {
+            this.uniqueId = Guid.NewGuid().ToString();
+
+            this.Evaluacion = new List<Evaluacion>(){};
+
+        }
     }
 }
