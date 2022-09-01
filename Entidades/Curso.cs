9c05f71fca915091ed7851294707ea151c2e548e
Jonathan Rodriguez Ramirez <jonathan.voices@gmail.com>
2022-09-01 13:38:45 -0500

    Implementacion de interfaces

--- Entidades/Curso.cs
+++ Entidades/Curso.cs
@@ -2,13 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreEscuela.Util;
 
 namespace CoreEscuela.Entidades;
 
-public class Curso : ObjetoEscuelaBase
+public class Curso : ObjetoEscuelaBase, ILugar
 {
     public TiposJornada jornada { get; set; }
     public List<Asignatura> Asignaturas{get;set;}
     public List<Alumno> Alumnos{get;set;}
 
+    public string direccion { get; set; }
+
+    public void limpiarLugar()
+    {
+        Printer.DrawLine();
+        Console.WriteLine("Limpiando Establecimiento...");
+        Console.WriteLine($"Curso {nombre} Limpio");
+    }
 }
